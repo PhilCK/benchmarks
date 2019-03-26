589c925608291442396fd38463771d9c54dd25f7
Phil Cooper-King <phil@cooperking.net>
2019-03-26 19:26:51 +0000

    added some alts

--- bench_err_check.c
+++ bench_err_check.c
@@ -22,18 +22,18 @@
  * Mixed Input Results
  * -------------------
  *
- *  Platform | Branches | Branch Tree | Error Table 
- * ==========|==========|=============|==============
- *  1.       | 352      | 186         | 380         
- *  2.       | 711      | 1362        | 327         
+ *  Platform | Branches | Unlikely | Giant | Branch Tree | Error Table 
+ * ==========|==========|==========|=======|=============|============
+ *  1.       | 352      | xxx      | xxx   | 186         | 380         
+ *  2.       | 660      | 564      | 636   | 837         | 375         
  *
  * Valid Input Results
  * -------------------
  *
- *  Platform | Branches | Branch Tree | Error Table 
- * ==========|==========|=============|==============
- *  1.       | 154      | 141         | 196         
- *  2.       | 630      | 420         | 336         
+ *  Platform | Branches | Unlikely | Giant | Branch Tree | Error Table 
+ * ==========|==========|==========|=======|=============|============
+ *  1.       | 154      | xxx      | xxx   | 141         | 196         
+ *  2.       | 369      | 306      | 258   | 432         | 357         
  *
  */
 
@@ -120,6 +120,9 @@ struct env valid_inputs[] = {
 uint64_t valid_input_count = (sizeof(valid_inputs) / sizeof(valid_inputs[0]));
 
 
+#define likely(x)       __builtin_expect((x),1)
+#define unlikely(x)     __builtin_expect((x),0)
+
 uint64_t
 get_time_rdtsc() {
   return __builtin_ia32_rdtsc();  
@@ -178,6 +181,91 @@ bench_error_branches(
   return end - start;
 }
 
+
+/* checks inputs with indivual if statements */
+uint64_t
+bench_error_giant_check(
+        struct env *inputs,
+        uint64_t input_count) 
+{
+        uint64_t i;
+        uint64_t invalid = 0;
+        uint64_t valid = 0;
+        uint64_t start = get_time_rdtsc();
+
+        for(i = 0; i < input_count; ++i) {
+                if((inputs[i].top_left_x > inputs[i].bot_right_x) ||
+                   (inputs[i].top_left_y > inputs[i].bot_right_y) ||
+                   (inputs[i].top_left_x > 100) ||
+                   (inputs[i].bot_right_x > 100) ||
+                   (inputs[i].top_left_y > 100) ||
+                   (inputs[i].bot_right_y > 100)) {
+                        invalid += 1;
+                        continue;
+                }
+
+                valid += 1;
+        }
+
+  uint64_t end = get_time_rdtsc();
+
+  printf("Valid/Invalid: %llu %llu\n", valid, invalid);
+  
+  return end - start;
+}
+
+
+/* checks inputs using hints */
+uint64_t
+bench_error_unlikely_branches(
+        struct env *inputs,
+        uint64_t input_count) 
+{
+        uint64_t i;
+        uint64_t invalid = 0;
+        uint64_t valid = 0;
+        uint64_t start = get_time_rdtsc();
+
+        for(i = 0; i < input_count; ++i) {
+                if(unlikely(inputs[i].top_left_x > inputs[i].bot_right_x)) {
+                        invalid += 1;
+                        continue;
+                }
+
+                else if(unlikely(inputs[i].top_left_y > inputs[i].bot_right_y)) {
+                        invalid += 1;
+                        continue;
+                }
+
+                else if(unlikely(inputs[i].top_left_x > 100)) {
+                        invalid += 1;
+                        continue;
+                }
+
+                else if(unlikely(inputs[i].bot_right_x > 100)) {
+                        invalid += 1;
+                        continue;
+                }
+
+                else if(unlikely(inputs[i].top_left_y > 100)) {
+                        invalid += 1;
+                        continue;
+                }
+
+                else if(unlikely(inputs[i].bot_right_y > 100)) {
+                        invalid += 1;
+                        continue;
+                }
+
+                valid += 1;
+        }
+
+  uint64_t end = get_time_rdtsc();
+
+  printf("Valid/Invalid: %llu %llu\n", valid, invalid);
+  
+  return end - start;
+}
 
 /* checks inputs with one large if/else if block */
 uint64_t
@@ -276,12 +364,16 @@ main() {
   printf("Mixed Inputs\n");
   printf("============\n");
   printf("branches: %llu\n--\n", bench_error_branches(mixed_inputs, mixed_input_count));
+  printf("unlikely branches: %llu\n--\n", bench_error_unlikely_branches(mixed_inputs, mixed_input_count));
+  printf("giant check: %llu\n--\n", bench_error_giant_check(mixed_inputs, mixed_input_count));
   printf("branch tree: %llu\n--\n", bench_error_branch_tree(mixed_inputs, mixed_input_count));
   printf("Error Table: %llu\n--\n", bench_error_table(mixed_inputs, mixed_input_count));
   
   printf("\nValid Inputs\n");
   printf("============\n");
   printf("branches: %llu\n--\n", bench_error_branches(valid_inputs, valid_input_count));
+  printf("unlikely branches: %llu\n--\n", bench_error_unlikely_branches(valid_inputs, valid_input_count));
+  printf("giant check: %llu\n--\n", bench_error_giant_check(valid_inputs, valid_input_count));
   printf("branch tree: %llu\n--\n", bench_error_branch_tree(valid_inputs, valid_input_count));
   printf("Error Table: %llu\n--\n", bench_error_table(valid_inputs, valid_input_count));
 
